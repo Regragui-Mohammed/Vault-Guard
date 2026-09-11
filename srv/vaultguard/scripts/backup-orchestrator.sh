baf4aa9ec20cce50ef1682bedfbba5e5762c2599
root <root@simo.simo>
2026-09-11 16:05:52 +0100

    Step 4: Optimize

--- srv/vaultguard/scripts/backup-orchestrator.sh
+++ srv/vaultguard/scripts/backup-orchestrator.sh
@@ -12,7 +12,23 @@ NEW_BACKUP_DIR="$STAGING_DIR/backup-$TIMESTAMP"
 LATEST_LINK="$STAGING_DIR/latest"
 TAR_FILE="$ARCHIVES_DIR/backup-$TIMESTAMP.tar"
 
-echo "[INFO] Syncing Data with rsync..."
+# =====Function======
+log_msg() {
+        local MSG=$1
+        echo "$MSG"
+        logger -t Vault-Guard "$MSG"
+}
+cleanup(){
+	log_msg "[INFO] Cleaning Up..."
+	if [ -f "$TAR_FILE" ]; then
+		rm -f "$TAR_FILE"
+	fi
+}
+trap cleanup EXIT ERR INT
+
+#===================
+
+log_msg "[INFO] Syncing Data with rsync..."
 if [ -d "$LATEST_LINK" ]; then
 	rsync -a --delete --link-dest="$LATEST_LINK" "$SOURCE_DIR/" "$NEW_BACKUP_DIR/"
 else
@@ -21,25 +37,24 @@ fi
 rm -f "$LATEST_LINK"
 ln -s "$NEW_BACKUP_DIR" "$LATEST_LINK"
 
-echo "[INFO] Creating Tar Archive..."
+log_msg "[INFO] Creating Tar Archive..."
 tar -cf "$TAR_FILE" -C "$STAGING_DIR" "backup-$TIMESTAMP"
 
-echo "[SUCCESS] Backup Engine completed Successfully."
-echo " Archive saved at : $TAR_FILE" 
+log_msg "[SUCCESS] Backup Engine completed Successfully."
+log_msg " Archive saved at : $TAR_FILE" 
 
 GPG_RECIPIENT="VaultGuard"
 
-echo "[INFO] Encrypting Archive with GPG..."
+log_msg "[INFO] Encrypting Archive with GPG..."
 gpg --batch --yes --trust-model always -r "$GPG_RECIPIENT" -e "$TAR_FILE"
 
-echo "[INFO] Generating SHA256 CheckSum ..."
+log_msg "[INFO] Generating SHA256 CheckSum ..."
 sha256sum "$TAR_FILE.gpg" > "$TAR_FILE.gpg.sha256"
 
-echo "[INFO] Applying Immutability Lock..."
+log_msg "[INFO] Applying Immutability Lock..."
 sudo chattr +i "$TAR_FILE.gpg"
 sudo chattr +i "$TAR_FILE.gpg.sha256"
 
 rm -f "$TAR_FILE"
 
-echo "[SUCCESS] Vault-Guard Backup Completed + Encrypted + Locked " 
-
+log_msg "[SUCCESS] Vault-Guard Backup Completed + Encrypted + Locked " 
